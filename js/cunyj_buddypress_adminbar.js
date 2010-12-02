05b44b7abc88fdd257d2c34ee406e4730befe25e
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-12-02 22:07:00 +0000

    Ignore the important bit, we're adding it directly to the element

--- js/cunyj_buddypress_adminbar.js
+++ js/cunyj_buddypress_adminbar.js
@@ -4,8 +4,8 @@ jQuery(document).ready(function() {
 	 * Insert styles if the admin bar is there. Solves problem with cache conflict
 	 */
 	if ( jQuery('#wp-admin-bar').length > 0 ) {
-		jQuery('body').css('padding-top', '30px !important');
-		jQuery('body').css('background-position', '0px 30px !important');	
+		jQuery('body').css('padding-top', '30px');
+		jQuery('body').css('background-position', '0px 30px');	
 	}
 	
 });
