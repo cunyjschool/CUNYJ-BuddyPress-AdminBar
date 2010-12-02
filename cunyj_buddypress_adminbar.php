316d2ddfc1fbe25da4abc4960aa94d5e62f93be5
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-12-02 22:17:45 +0000

    Tagging v0.3.3

--- cunyj_buddypress_adminbar.php
+++ cunyj_buddypress_adminbar.php
@@ -1,14 +1,14 @@
 <?php
 /*
  * Plugin Name: CUNYJ BuddyPress Admin Bar
- * Version: 0.3.2
+ * Version: 0.3.3
  * Plugin URI: http://journalism.cuny.edu
  * Description: Customized Admin Bar up in the heezy
  * Author: Daniel Bachhuber
  * Author URI: http://www.danielbachhuber.com/
  */
 
-define( 'CUNYJ_BUDDYPRESS_ADMIN_BAR_VERSION', "0.3.2" );
+define( 'CUNYJ_BUDDYPRESS_ADMIN_BAR_VERSION', "0.3.3" );
 
 class cunyj_buddypress
 {
